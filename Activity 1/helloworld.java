b7b4f6eef75144ca027ed992b08d84eddd85188c
alexdlr <alexdlr@gmail.com>
2015-04-29 09:29:54 -0500

    First Activity

---
+++ Activity 1/helloworld.java
@@ -0,0 +1,10 @@
+/*******************************************
+* Hello World
+* First Practice
+* ADLR 04 28 2015
+*******************************************/
+public class helloworld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
